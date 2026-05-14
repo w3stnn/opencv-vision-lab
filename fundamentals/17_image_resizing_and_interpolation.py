dac0e4c34b9941579eb4a24a508367b6cfb38fd6
Batın TAŞKIN <batintaskin7@gmail.com>
2026-05-14 17:22:16 +0300

    Add Lesson 17: Image resizing with cubic interpolation
    Görüntü boyutlandırma işlemleri katsayılar (fx, fy) kullanılarak örneklendi. Büyütme işlemlerinde kaliteyi korumak için INTER_CUBIC interpolasyon yöntemi uygulandı

--- fundamentals/17_image_resizing_and_interpolation.py
+++ fundamentals/17_image_resizing_and_interpolation.py
@@ -1,14 +1,13 @@
 import cv2
-#yeniden boyutlandırma
-img= cv2.imread("assets/highway_test_image.jpg")
 
-#print(img.shape)  #(1536, 2048, 3) pixel olduğunu öğrendik
-# res = cv2.resize(img,(300,300))  #burada kendimiz manuel şekilde ayarladık
-#res = cv2.resize(img,None,fx=1.4,)
-res = cv2.resize(img,None,fx=1.4,fy=1,interpolation=cv2.INTER_CUBIC)
+img=cv2.imread("assets/curved_road_test.jpg")
 
+print(img.shape) #görselin boyutunu öğrendik == (4032, 2268, 3)
+#res= cv2.resize(img,(100,100))
+res = cv2.resize(img,None,fx=1.4,fy=0.5,interpolation=cv2.INTER_CUBIC)
 
-cv2.imshow("res",res)
 cv2.imshow("img",img)
-cv2.waitKey()
+cv2.imshow("res",res)
+
+cv2.waitKey(0)
 cv2.destroyAllWindows()
